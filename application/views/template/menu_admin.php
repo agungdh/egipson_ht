dae3bb790bff7d378e0bd7d727c27ee0e0bd7d00
agungdh <agungdh@live.com>
2018-05-22 02:00:12 +0700

    update

--- application/views/template/menu_admin.php
+++ application/views/template/menu_admin.php
@@ -1,7 +1,7 @@
 <li><a class="app-menu__item" href="<?php echo base_url(); ?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
 
-<li><a class="app-menu__item" href="<?php echo base_url('peminjaman'); ?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Peminjaman</span></a></li>
+<li><a class="app-menu__item" href="<?php echo base_url('peminjaman'); ?>"><i class="app-menu__icon fa fa-book"></i><span class="app-menu__label">Peminjaman</span></a></li>
 
-<li><a class="app-menu__item" href="<?php echo base_url('pengembalian'); ?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Pengembalian</span></a></li>
+<li><a class="app-menu__item" href="<?php echo base_url('pengembalian'); ?>"><i class="app-menu__icon fa fa-book"></i><span class="app-menu__label">Pengembalian</span></a></li>
 
 <li><a class="app-menu__item" href="<?php echo base_url('user'); ?>"><i class="app-menu__icon fa fa-users"></i><span class="app-menu__label">User</span></a></li>
