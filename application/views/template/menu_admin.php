c688e2a4129401ae1802ce446c9d8a52a1bc0251
agungdh <agungdh@live.com>
2018-05-22 11:11:52 +0700

    update

--- application/views/template/menu_admin.php
+++ application/views/template/menu_admin.php
@@ -7,3 +7,5 @@
 <li><a class="app-menu__item" href="<?php echo base_url('user'); ?>"><i class="app-menu__icon fa fa-users"></i><span class="app-menu__label">User</span></a></li>
 
 <li><a class="app-menu__item" href="<?php echo base_url('config'); ?>"><i class="app-menu__icon fa fa-gears"></i><span class="app-menu__label">Config</span></a></li>
+
+<li><a class="app-menu__item" href="<?php echo base_url('log'); ?>"><i class="app-menu__icon fa fa-info-circle"></i><span class="app-menu__label">Log</span></a></li>
