e90b00fc1c38268b299521ab5e113daded23c3c5
agungdh <agungdh@live.com>
2018-05-27 12:10:16 +0700

    update

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -3,6 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $ci = new CI_Controller();
 $ci =& get_instance();
+
+$now = date('YmdHis');
 ?>
 
 <!DOCTYPE html>
@@ -10,8 +12,18 @@ $ci =& get_instance();
 
 <head>
   <meta charset="UTF-8">
-  <title>Error 404</title>
+  <title>404 Page Not Found</title>
   
+  	    <!-- Favicon -->
+	    <?php
+	    if (file_exists('uploads/favicon')) {
+	      $favicon = 'uploads/favicon';
+	    } else {
+	      $favicon = 'assets/favicon.png';
+	    }
+	    ?>
+  	 	<link rel="shortcut icon" href="<?php echo base_url($favicon) . '?time=' . $now; ?>"/>
+
       <link rel="stylesheet" href="<?php echo base_url('assets/404/css/bootstrap.min.css'); ?>">
 
       <link rel="stylesheet" href="<?php echo base_url('assets/404/css/style.css'); ?>">
