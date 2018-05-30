d8a83d114ff016c2f59cdb50a1d7ffd44734257c
agungdh <agungdh@live.com>
2018-05-30 10:01:32 +0700

    tambah lib smarty

---
+++ application/libraries/Smartyci.php
@@ -0,0 +1,27 @@
+<?php
+class Smartyci extends Smarty
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $config =& get_config();
+
+        // $this->caching = 1;
+        $this->setTemplateDir( APPPATH . 'views' );
+        $this->setCompileDir( APPPATH . 'views/compiled' );
+        $this->setConfigDir( APPPATH . 'config/smarty' );
+        $this->setCacheDir( APPPATH . 'cache' );
+    }
+
+    //if specified template is cached then display template and exit, otherwise, do nothing.
+    public function useCached( $tpl, $cacheId = null )
+    {
+        if ( $this->isCached( $tpl, $cacheId ) )
+        {
+            $this->display( $tpl, $cacheId );
+            exit();
+        }
+    }
+}
+?>
